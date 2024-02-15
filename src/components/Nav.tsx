7e5fc5a607b678c385c9071517e1aadde4d781c9
Jaelyn Lee <leejay1020@gmail.com>
2024-02-15 19:15:15 +1300

    update Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,6 +2,29 @@ import { motion } from 'framer-motion'
 import { useState } from 'react'
 import { useMediaQuery } from 'usehooks-ts'
 
+const navMotion = {
+  visible: {
+    opacity: 1,
+    transition: {
+      when: 'beforeChildren',
+      staggerChildren: 0.3,
+    },
+  },
+  hidden: {
+    opacity: 0,
+  },
+}
+const itemMotion = {
+  visible: {
+    opacity: 1,
+    x: 0,
+  },
+  hidden: {
+    opacity: 0,
+    x: -180,
+  },
+}
+
 export default function Nav() {
   const [toggled, setToggled] = useState(false)
   const matches = useMediaQuery('(min-width: 1280px)')
@@ -24,21 +47,49 @@ export default function Nav() {
       {!matches && (
         <div
           onClick={() => setToggled((preToggle) => !preToggle)}
-          className="space-y-1 cursor-pointer z-50"
+          className="space-y-1.5 cursor-pointer z-50"
         >
-          <span className="block h-0.5 w-8 bg-black"></span>
-          <span className="block h-0.5 w-6 bg-black"></span>
-          <span className="block h-0.5 w-4 bg-black"></span>
+          <motion.span
+            animate={{ rotateZ: toggled ? 45 : 0, y: toggled ? 8 : 0 }}
+            className="block h-0.5 w-8 bg-black"
+          ></motion.span>
+          <motion.span
+            animate={{ width: toggled ? 0 : 24 }}
+            className="block h-0.5 w-6 bg-black"
+          ></motion.span>
+          <motion.span
+            animate={{
+              rotateZ: toggled ? -45 : 0,
+              y: toggled ? -8 : 0,
+              width: toggled ? 32 : 16,
+            }}
+            className="block h-0.5 w-4 bg-black"
+          ></motion.span>
         </div>
       )}
       {toggled && !matches && (
-        <div className="flex items-center justify-center fixed bg-white bottom-0 left-0 w-full h-screen">
-          <div className="flex flex-col gap-24 text-lg">
-            <a href="/">Home</a>
-            <a href="/services">Services</a>
-            <a href="/contact">Contact</a>
-          </div>
-        </div>
+        <motion.div
+          animate={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 25 }}
+          className="flex items-center justify-center fixed bg-white bottom-0 left-0 w-full h-screen"
+        >
+          <motion.div
+            variants={navMotion}
+            animate="visible"
+            initial="hidden"
+            className="flex flex-col gap-24 text-lg"
+          >
+            <motion.a variants={itemMotion} href="/">
+              Home
+            </motion.a>
+            <motion.a variants={itemMotion} href="/services">
+              Services
+            </motion.a>
+            <motion.a variants={itemMotion} href="/contact">
+              Contact
+            </motion.a>
+          </motion.div>
+        </motion.div>
       )}
     </nav>
   )
